6183e071099a7b5aa6a830c837d99b0c2beba80a
ivoosantos <oliveiradossantosivo@gmail.com>
2020-04-10 00:21:37 -0300

    Projeto de ML.Net para reconhecimento de alimentos ou brinquedos em uma imagem.

---
+++ ImageData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransferLearningTF
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string ImagePath;
+
+        [LoadColumn(1)]
+        public string Label;
+    }
+}
